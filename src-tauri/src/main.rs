fef2045da89a5ff71e7884aa020a19aa5f205918
Gustavo Meira <gmeira.antonio@gmail.com>
2024-02-02 08:21:06 -0300

    add tauri plugin sql

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri_plugin_sql::{Migration, MigrationKind};
 
 fn main() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create_initial_tables",
+            sql: "CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, completed INTEGER);",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "populate_todo",
+            sql: "INSERT INTO todo (text, completed) VALUES ('Fazer compras', 0), ('Estudar SQL', 1), ('Exercitar-se', 0);",
+            kind: MigrationKind::Up,
+        },
+    ];
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_sql::Builder::default().add_migrations("sqlite:test.db", migrations).build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,86 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import { invoke } from '@tauri-apps/api/tauri';
+import './App.css';
+import Database from 'tauri-plugin-sql-api';
+
+type DatabaseTodo = {
+  id: number;
+  title: string;
+  completed: number;
+};
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [todoText, setTodoText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const createTodo = async () => {
+    const db = await Database.load('sqlite:test.db');
+
+    const newTodo = await db.execute(
+      'INSERT INTO todo (text, completed) VALUES (?, ?)',
+      [todoText, 0]
+    );
+
+    setTodos([
+      ...todos,
+      { id: newTodo.lastInsertId, title: todoText, completed: false },
+    ]);
+    setTodoText('');
+  };
+
+  useEffect(() => {
+    (async () => {
+      const db = await Database.load('sqlite:test.db');
+
+      const todos = await db.select<DatabaseTodo[]>('SELECT * FROM todo');
+
+      setTodos(
+        todos.map((todo) => ({ ...todo, completed: Boolean(todo.completed) }))
+      );
+    })();
+  }, []);
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+        >
+          <img
+            src="/vite.svg"
+            className="logo vite"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        <a
+          href="https://tauri.app"
+          target="_blank"
+        >
+          <img
+            src="/tauri.svg"
+            className="logo tauri"
+            alt="Tauri logo"
+          />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
 
@@ -34,18 +90,18 @@ function App() {
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          createTodo();
         }}
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          onChange={(e) => setTodoText(e.currentTarget.value)}
+          placeholder="Enter a todo..."
         />
         <button type="submit">Greet</button>
       </form>
 
-      <p>{greetMsg}</p>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
     </div>
   );
 }
